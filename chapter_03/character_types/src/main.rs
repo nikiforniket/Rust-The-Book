ebb2ddb547a49aca3e9062ee7f01702311071f91
nikiforniket <nikeetkumar7857@gmail.com>
2025-06-07 22:01:31 +0530

    Added character data type

---
+++ chapter_03/character_types/src/main.rs
@@ -0,0 +1,7 @@
+fn main () {
+    // specify by single quote (double quote is used for sting literals), Size is 4 bytes
+    let c = 'x';
+    let my_name_first_letter:char = 'n'; // with type annotations;
+
+    println!("Simple char variable name c : {c}, my name first letter {my_name_first_letter}");
+}
