b7c18497720b448ef025d93b5d7552b84fdfda43
nikiforniket <nikeetkumar7857@gmail.com>
2025-06-07 12:41:25 +0530

    unsiged and signed integer types

---
+++ chapter_03/scalar_types/src/main.rs
@@ -0,0 +1,11 @@
+fn main () {
+
+    let num1_top :i8 = 127;
+    let num1_low :i8 = -128;
+    println!("The i8 type number : {num1_top}");
+    println!("The i8 type number : {num1_low}");
+
+    let num2_top :i16 = 3209;
+
+    let num3 : u8 = 255;
+}
