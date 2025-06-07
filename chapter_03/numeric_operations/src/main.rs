48ec51c151499413cfed200c67ea57839ef8b65a
nikiforniket <nikeetkumar7857@gmail.com>
2025-06-07 21:46:10 +0530

    Basic numeric operations

---
+++ chapter_03/numeric_operations/src/main.rs
@@ -0,0 +1,19 @@
+fn main () {
+    let sum :i8 = 4 + 5;
+    println!("Sum is {sum} of 4 and 5");
+
+    let diff : i8 = 10 - 8;
+    println!("Diff is {diff} of 10 and 8");
+
+    let multiplication : i8 = 10 * 5;
+    println!("Multiplication is {multiplication} of 10 and 5");
+
+    let quotient:f32 = 5.7 / 2.1;
+    println!("Quotient is {quotient} of 5.7/2.1");
+
+    let quotient_integer:i8 = 4 / 2;
+    println!("Quotient integer is {quotient_integer} of 4 % 2");
+
+    let remainder:i8 = 5 % 2;
+    println!("Remainder is {remainder} of 5 % 2");
+}
